f570cac877900ea0e936591bdfcb69a08d0e3771
auuo <9277206@qq.com>
2021-12-27 20:51:27 +0800

    refactor: log id format ip

--- src/log_id.rs
+++ src/log_id.rs
@@ -1,25 +1,29 @@
 use std::net::{IpAddr, Ipv6Addr, UdpSocket};
 use chrono::Utc;
+use lazy_static::lazy_static;
 
 const VERSION: &'static str = "01";
 
+lazy_static! {
+    static ref FORMAT_IP: String = {
+        let v6 = match local_ip() {
+            Some(IpAddr::V4(v4)) => v4.to_ipv6_mapped(),
+            Some(IpAddr::V6(v6)) => v6,
+            None => Ipv6Addr::from(0),
+        };
+        format!("{:032x}", u128::from(v6))
+    }
+}
+
 /// 生成一个用于追踪作用的 log id
+/// 格式: 2 位版本号 + 13 位时间戳 + 32 位 ip + 6 位随机数 = 53 位
 pub fn gen_log_id() -> String {
     let mut id = String::new();
     id.push_str(VERSION);
     id.push_str(&Utc::now().timestamp_millis().to_string());
-    // local ip 编码为 32 位长度的十六进制
+    id.push_str(&FORMAT_IP);
     // 随机数，转 16 进制
     id
-}
-
-fn a() -> String {
-    let v6 = match local_ip() {
-        Some(IpAddr::V4(v4)) => v4.to_ipv6_mapped(),
-        Some(IpAddr::V6(v6)) => v6,
-        None => Ipv6Addr::from(0),
-    };
-    format!("{:02x}", u128::from(v6))
 }
 
 /// 获得本地 ip，并不会真的发起请求，使用 udp 也不会有握手请求
@@ -35,6 +39,6 @@ fn test_get_local_ip() {
 }
 
 #[test]
-fn test_get_ip_hex() {
-    println!("{}", a())
+fn test_gen_log_id() {
+    println!("{}", gen_log_id);
 }
