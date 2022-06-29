1cb292c768e3cbe2680bd114864e4586689ec929
auuo <9277206@gmail.com>
2022-06-29 17:12:46 +0800

    refactor: define_error 去除对 std::error::Error 的实现，支持所有 Error 直接转换

--- aux-error/src/lib.rs
+++ aux-error/src/lib.rs
@@ -24,15 +24,13 @@
         }
     ) => {
         $(#[$docs])*
-        #[derive(Debug, thiserror::Error)]
+        #[derive(Debug)]
         $vis enum $enum_name {
             $(
-                #[error("{}", stringify!($name))]
                 $name(Option<$args_type>),
             )+
 
-            #[error(transparent)]
-            Unknown(#[from] anyhow::Error),
+            Unknown(anyhow::Error),
         }
 
         impl $enum_name {
@@ -57,25 +55,13 @@
                 }
             }
         }
-    }
-}
 
-/// 用于定义可以 into 到 Unknown(anyhow::Error) 的类型
-///
-/// # Example
-///
-/// ```
-/// aux_error::define_error_from! {
-///     AppError(reqwest::Error) // 定义可从 reqwest::Error 类型转换到 Unknown 类型
-/// }
-///
-/// reqwest::get().await? // 可使用 ? 直接向上返回错误
-/// ```
-#[macro_export] macro_rules! define_error_from {
-    ($enum_name:ident($from_ty:ty);) => {
-        impl From<$from_ty> for $enum_name {
-            fn from(e: $from_ty) -> Self {
-                $enum_name::Unknown(e.into())
+        impl<T> From<T> for $enum_name
+        where
+            T: Into<anyhow::Error>
+        {
+            fn from(t: T) -> Self {
+                $enum_name::Unknown(t.into())
             }
         }
     }
